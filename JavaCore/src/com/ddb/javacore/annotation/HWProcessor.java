c04d4f6560825cdbddeb8b504fdbce1c95fd5563
wangyuchen <chen_841460210@qq.com>
2016-11-10 16:27:23 +0800

    ddd

--- JavaCore/src/com/ddb/javacore/annotation/HWProcessor.java
+++ JavaCore/src/com/ddb/javacore/annotation/HWProcessor.java
@@ -6,14 +6,16 @@ import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.element.TypeElement;
 
+
 public class HWProcessor extends AbstractProcessor {
 
 	@Override
 	public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
 		// TODO Auto-generated method stub
-	
-		
 		return false;
 	}
 
+    
+    
+    
 }
